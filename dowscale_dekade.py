b1a7e472d8ac2ec83742dc9173f2ab6484f988c5
uruja <uruja@student.kit.edu>
2026-03-11 09:09:58 +0100

    Update dowscale_dekade.py

--- dowscale_dekade.py
+++ dowscale_dekade.py
@@ -1,96 +1,100 @@
-import get_ECMWF_functions as gef
-import xarray as xr
-import numpy as np
-import matplotlib.pyplot as plt
-import geopandas as gpd
-import rioxarray
-from matplotlib.colors import LinearSegmentedColormap
-from datetime import datetime, timedelta
+# import get_ECMWF_functions as gef
+# import xarray as xr
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import geopandas as gpd
+# import rioxarray
+# from matplotlib.colors import LinearSegmentedColormap
+# from datetime import datetime, timedelta
 
-today = datetime.today()
-two_days_earlier = today - timedelta(days=2)
-date_str = two_days_earlier.strftime("%Y-%m-%d")
+# today = datetime.today()
+# two_days_earlier = today - timedelta(days=2)
+# date_str = two_days_earlier.strftime("%Y-%m-%d")
 
-data_dekade=xr.open_dataset(f'data/{date_str}/data_dekade.nc')
-month=int(data_dekade.time.dt.month.values)
-day=int(data_dekade.time.dt.day.values)
+# data_dekade=xr.open_dataset(f'data/{date_str}/data_dekade.nc')
+# month=int(data_dekade.time.dt.month.values)
+# day=int(data_dekade.time.dt.day.values)
 
-forecast_files = {
-    (2, 17): ["ECMWF_tp_forecasts_02-17-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_06_Kenya.nc"],
-    (2, 27): ["ECMWF_tp_forecasts_02-27-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_07_Kenya.nc"],
-    (3, 9): ["ECMWF_tp_forecasts_03-09-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_08_Kenya.nc"],
-    (3, 19): ["ECMWF_tp_forecasts_03-19-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_09_Kenya.nc"],
-    (3, 31): ["ECMWF_tp_forecasts_03-31-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_10_Kenya.nc"],
-    (4, 9): ["ECMWF_tp_forecasts_04-09-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_11_Kenya.nc"],
-    (4, 19): ["ECMWF_tp_forecasts_04-19-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_12_Kenya.nc"],
-    (4, 29): ["ECMWF_tp_forecasts_04-29-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_13_Kenya.nc"],
-}
+# forecast_files = {
+#     (2, 17): ["ECMWF_tp_forecasts_02-17-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_06_Kenya.nc"],
+#     (2, 27): ["ECMWF_tp_forecasts_02-27-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_07_Kenya.nc"],
+#     (3, 9): ["ECMWF_tp_forecasts_03-09-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_08_Kenya.nc"],
+#     (3, 19): ["ECMWF_tp_forecasts_03-19-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_09_Kenya.nc"],
+#     (3, 31): ["ECMWF_tp_forecasts_03-31-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_10_Kenya.nc"],
+#     (4, 9): ["ECMWF_tp_forecasts_04-09-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_11_Kenya.nc"],
+#     (4, 19): ["ECMWF_tp_forecasts_04-19-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_12_Kenya.nc"],
+#     (4, 29): ["ECMWF_tp_forecasts_04-29-2025_day2_to_day11_Kenya.nc","chirpsv3_dekads_2005_2025_sorted_13_Kenya.nc"],
+# }
 
-try:
-    keys = list(forecast_files)
-    start = keys.index((month, day))
-    fclim_chirps = np.array([forecast_files[k] for k in keys[start:start+4]]).T
+# try:
+#     keys = list(forecast_files)
+#     start = keys.index((month, day))
+#     fclim_chirps = np.array([forecast_files[k] for k in keys[start:start+4]]).T
 
-    reforecast_clims=[]
-    for i,file in enumerate(fclim_chirps[0]):
-        fclim=xr.open_dataset('downscale_data/'+file)
-        reforecast_clims.append(fclim.assign_coords({'step':data_dekade.step.values[i]}))
-    reforecast_clims_ds=xr.concat(reforecast_clims,dim='step')
+#     reforecast_clims=[]
+#     for i,file in enumerate(fclim_chirps[0]):
+#         fclim=xr.open_dataset('downscale_data/'+file)
+#         reforecast_clims.append(fclim.assign_coords({'step':data_dekade.step.values[i]}))
+#     reforecast_clims_ds=xr.concat(reforecast_clims,dim='step')
 
                             
-    chirps_dekades=[]
-    for i,file in enumerate(fclim_chirps[1]):
-        chirps=xr.open_dataset('downscale_data/'+file)
-        chirps_dekades.append(chirps.assign_coords({'step':data_dekade.step.values[i]}))
-    chirps_dekades_ds=xr.concat(chirps_dekades,dim='step')
+#     chirps_dekades=[]
+#     for i,file in enumerate(fclim_chirps[1]):
+#         if file=="chirpsv3_dekads_2005_2025_sorted_11_Kenya.nc":
+#             file="chirpsv3_dekads_2005_2025_sorted_12_Kenya.nc"
+#             chirps=xr.open_dataset('downscale_data/'+file)*np.nan
+#         else:
+#          chirps=xr.open_dataset('downscale_data/'+file)
+#         chirps_dekades.append(chirps.assign_coords({'step':data_dekade.step.values[i]}))
+#     chirps_dekades_ds=xr.concat(chirps_dekades,dim='step')
 
-    bboxes = {
-        "Kenya": {"lat1": 7, "lon1": 33, "lat2": -6, "lon2": 42},
-        "kenya_plus":{"lat1": 7.5, "lon1":27, "lat2": -7.5, "lon2": 43}
-    }
+#     bboxes = {
+#         "Kenya": {"lat1": 7, "lon1": 33, "lat2": -6, "lon2": 42},
+#         "kenya_plus":{"lat1": 7.5, "lon1":27, "lat2": -7.5, "lon2": 43}
+#     }
 
-    country="kenya_plus"
-    data_to_add=data_dekade.assign_coords({"year":int(data_dekade.time.dt.year.values)}).mean('number').sel(longitude=slice(bboxes[country]['lon1'],bboxes[country]['lon2']),latitude=slice(bboxes[country]['lat1'],bboxes[country]['lat2']))
-    extended_fclim=xr.concat([reforecast_clims_ds,data_to_add],dim='year')
+#     country="kenya_plus"
+#     data_to_add=data_dekade.assign_coords({"year":int(data_dekade.time.dt.year.values)}).mean('number').sel(longitude=slice(bboxes[country]['lon1'],bboxes[country]['lon2']),latitude=slice(bboxes[country]['lat1'],bboxes[country]['lat2']))
+#     extended_fclim=xr.concat([reforecast_clims_ds,data_to_add],dim='year')
 
-    rescaled_forecast=gef.rank_upscale_and_align(extended_fclim.tp,chirps_dekades_ds.tp)
-    rescaled_forecast=rescaled_forecast.assign_coords({'time':extended_fclim.time,'valid_time':extended_fclim.valid_time}).to_dataset(name='tp')
-    rescaled_forecast.tp.attrs=data_dekade.tp.attrs
+#     rescaled_forecast=gef.rank_upscale_and_align(extended_fclim.tp,chirps_dekades_ds.tp)
+#     rescaled_forecast=rescaled_forecast.assign_coords({'time':extended_fclim.time,'valid_time':extended_fclim.valid_time}).to_dataset(name='tp')
+#     rescaled_forecast.tp.attrs=data_dekade.tp.attrs
 
-    rescaled_forecast.to_netcdf(f'data/{date_str}/data_dekade_downscaled.nc')
+#     rescaled_forecast.to_netcdf(f'data/{date_str}/data_dekade_downscaled.nc')
 
-    fs=12
-    country='Kenya'
+#     fs=12
+#     country='Kenya'
 
-    gef.lat1=bboxes[country]['lat1']
-    gef.lat2=bboxes[country]['lat2']
-    gef.lon1=bboxes[country]['lon1']
-    gef.lon2=bboxes[country]['lon2']
+#     gef.lat1=bboxes[country]['lat1']
+#     gef.lat2=bboxes[country]['lat2']
+#     gef.lon1=bboxes[country]['lon1']
+#     gef.lon2=bboxes[country]['lon2']
 
-    cmap=gef.cmap
+#     cmap=gef.cmap
 
-    ds_to_plot=rescaled_forecast.sel(longitude=slice(bboxes[country]['lon1'],bboxes[country]['lon2']),latitude=slice(bboxes[country]['lat1'],bboxes[country]['lat2']))
-    fig=gef.panel_plot_variable(ds_to_plot,variable='tp',forecast_timestep=ds_to_plot.step.values,cmap=cmap,fontsize=fs,vmax=int(ds_to_plot.quantile(0.99).tp.values))
-    plt.savefig(f'plots/{country}/{date_str}/dekadal/dekadal_precip_downscaled.png',bbox_inches='tight')
+#     ds_to_plot=rescaled_forecast.sel(longitude=slice(bboxes[country]['lon1'],bboxes[country]['lon2']),latitude=slice(bboxes[country]['lat1'],bboxes[country]['lat2']))
+#     fig=gef.panel_plot_variable(ds_to_plot,variable='tp',forecast_timestep=ds_to_plot.step.values,cmap=cmap,fontsize=fs,vmax=int(ds_to_plot.quantile(0.99).tp.values))
+#     plt.savefig(f'plots/{country}/{date_str}/dekadal/dekadal_precip_downscaled.png',bbox_inches='tight')
 
-    dirname=f'data/{date_str}/geotifs_kenya/'
-    os.mkdir(dirname)
+#     dirname=f'data/{date_str}/geotifs_kenya/'
+#     os.mkdir(dirname)
     
-    for i in range(4):
-        fname=f'forecast_init_{str(ds_to_plot.isel(step=0).valid_time.values-dt)[5:10]}_start_{str(ds_to_plot.isel(step=i).valid_time.values-dt)[5:10]}_end_{str(ds_to_plot.isel(step=i).valid_time.values)[5:10]}.tif'
-        ds_to_plot.isel(step=i).rio.to_raster(dirname+fname)    
+#     for i in range(4):
+#         fname=f'forecast_init_{str(ds_to_plot.isel(step=0).valid_time.values-dt)[5:10]}_start_{str(ds_to_plot.isel(step=i).valid_time.values-dt)[5:10]}_end_{str(ds_to_plot.isel(step=i).valid_time.values)[5:10]}.tif'
+#         ds_to_plot.isel(step=i).rio.to_raster(dirname+fname)    
 
-    gdf = gpd.read_file("downscale_data/Kenya_Counties_KNSDI.shp").set_crs("EPSG:4326")
+#     gdf = gpd.read_file("downscale_data/Kenya_Counties_KNSDI.shp").set_crs("EPSG:4326")
 
-    rescaled_forecast = rescaled_forecast.rio.write_crs("EPSG:4326")
+#     rescaled_forecast = rescaled_forecast.rio.write_crs("EPSG:4326")
 
-    # Reproject shapefile
-    gdf = gdf.to_crs(rescaled_forecast.rio.crs)
+#     # Reproject shapefile
+#     gdf = gdf.to_crs(rescaled_forecast.rio.crs)
 
-    # Clip
-    ds_to_plot = rescaled_forecast.rio.clip(gdf.geometry, gdf.crs, drop=True)
-    fig=gef.panel_plot_variable(ds_to_plot,variable='tp',forecast_timestep=ds_to_plot.step.values,cmap=cmap,fontsize=fs,vmax=int(ds_to_plot.quantile(0.99).tp.values))
-    plt.savefig(f'plots/{country}/{date_str}/dekadal/dekadal_precip_downscaled_clipped.png',bbox_inches='tight')
+#     # Clip
+#     ds_to_plot = rescaled_forecast.rio.clip(gdf.geometry, gdf.crs, drop=True)
+#     fig=gef.panel_plot_variable(ds_to_plot,variable='tp',forecast_timestep=ds_to_plot.step.values,cmap=cmap,fontsize=fs,vmax=int(ds_to_plot.quantile(0.99).tp.values))
+#     plt.savefig(f'plots/{country}/{date_str}/dekadal/dekadal_precip_downscaled_clipped.png',bbox_inches='tight')
 
-except ValueError:
-    print('these are not the days you are looking for...')
+# except ValueError:
+#     print('these are not the days you are looking for...')
